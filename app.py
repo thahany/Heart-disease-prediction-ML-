e18420742300084b7655800dac360e514575d46a
thahany <thahanys@gmail.com>
2021-11-16 00:00:13 -0500

    app change

--- app.py
+++ app.py
@@ -38,35 +38,43 @@ def predict_class(df):
 
 def main():
     html_temp = """
-    <div style="background-color:red;padding:10px">
-    <h2 style="color:black;text-align:center;">Heart Disease Prediction</h2>
+    <div style="background-color:blue;padding:10px">
+    <h2 style="color:white;text-align:center;">Heart Disease Prediction</h2>
     </div>
     """
   
     st.markdown(html_temp , unsafe_allow_html= True)
-    image = Image.open('h1.png')
-    st.image(image, use_column_width=True,format='PNG')
+    image = Image.open('hdp.png')
+    # st.image(image, use_column_width=True,format='png')
+    st.image(image, use_column_width=True)
     
     
     
-    age=st.slider('Age', 0,100)
-    sex=st.text_input('Sex 1(male)-0(Female)',' ')
-    cp=st.text_input('Chest pain 0,1,2,3',' ')
-    trestbps=st.slider('Resting blood pressure in mm Hg ', 90,200)
-    chol=st.slider('Serum cholestoral in mg/dl', 126,564)
-    fbs=st.text_input('Fasting blood sugar &gt; 120 mg/dl, (1 = true; 0 = false)',' ')
-    restecg=st.slider('Resting electrocardiographic results', 0,2)
-    thalach=st.slider('Maximum heart rate achieved', 65,210)
+    age=st.text_input('Age')
+    # age=st.slider('Age', 0,100)
+    sex=st.text_input('Sex 1(M)-0(F)',' ')
+    cp=st.text_input('Chest pain (1 = typical angina; 2 = atypical angina; 3 = non-anginal pain; 4 = asymptomatic)',' ')
+    trestbps=st.text_input('Resting blood pressure in mm Hg ')
+    # trestbps=st.slider('Resting blood pressure in mm Hg ', 90,200)
+    chol=st.text_input('Serum cholestoral in mg/dl Range;126-564')
+    # chol=st.slider('Serum cholestoral in mg/dl', 126,564)
+    fbs=st.text_input('Fasting blood sugar > 120 mg/dl, (1 = true; 0 = false)',' ')
+    restecg=st.text_input('Resting electrocardiographic results (0 = normal; 1 = having ST-T; 2 = hypertrophy)','')
+    # restecg=st.slider('Resting electrocardiographic results (0 = normal; 1 = having ST-T; 2 = hypertrophy)','')
+    thalach=st.text_input('Maximum heart rate achieved',' ')
+    # thalach=st.slider('Maximum heart rate achieved', 65,210)
     exang=st.text_input('Exercise induced angina 1 = es; 0 = No',' ')
-    oldpeak=st.text_input('ST depression induced by exercise relative to rest (0-7)',' ')
-    slope=st.text_input('The slope of the peak exercise ST segment (0-2)', ' ')
+    # oldpeak=st.text_input('ST depression induced by exercise relative to rest (0-7)',' ')
+    slope=st.text_input('The slope of the peak exercise ST segment (1 = upsloping; 2 = flat; 3 = downsloping)', ' ')
     ca=st.text_input('Number of major vessels (0-3) colored by flourosopy', ' ')
-    thal=st.text_input('Thal (1 = normal, 2 = fixed defect, 3 = reversable defect)', ' ')
+    thal=st.text_input('Hemoglobin level (1 = normal, 2 = fixed defect, 3 = defect)', ' ')
               
     result =""
     if st.button('Predict'):
+        # dataset = [int(age),int(sex),int(cp),int(trestbps),int(chol),int(fbs),int(restecg),
+        #                       int(thalach),int(exang),float(oldpeak),int(ca),int(thal)]
         dataset = [int(age),int(sex),int(cp),int(trestbps),int(chol),int(fbs),int(restecg),
-                              int(thalach),int(exang),float(oldpeak),int(slope),int(ca),int(thal)]
+                              int(thalach),int(exang),int(slope),int(ca),int(thal)]
         df = pd.DataFrame(dataset)
         df_trans = standardScaler.fit_transform(df)
         df_trans = df_trans.T
@@ -79,12 +87,13 @@ def main():
   
     st.success('{}'.format(result))
     if st.button("About"):
-        st.text("A controlled carbohydrate lifestyle really prevents risk factors for heart disease.")
-        st.text("Github link: https://github.com/ayushkesh/Heart-disease-prediction-ML- ")   
+        st.text("BMG 5111F_EHR Project_Heart Disease Prediction.")
+        # st.text("A controlled carbohydrate lifestyle really prevents risk factors for heart disease.")
+        # st.text("Github link: https://github.com/ayushkesh/Heart-disease-prediction-ML- ")   
         
     html_temp1 = """
-    <div style="background-color:#f63366">
-    <p style="color:white;text-align:center;" >By: <b>Ayush Kumar</b> </p>
+    <div style="background-color:green;"> 
+    <p style="color:white;text-align:center;" >By: <b>Raouf & Thahany</b> </p>
     </div>
     """
     st.markdown(html_temp1,unsafe_allow_html=True)
